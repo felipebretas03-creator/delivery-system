a4b013b3e65ea685b12bd980a126e713adb7fcef
felipebretas03-creator <felipebretas03@gmail.com>
2026-04-14 16:04:12 -0300

    fix: garantindo rotas api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,43 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
-import bcrypt from 'bcryptjs';
-import { prisma } from './prisma';
 
 const app = express();
 
-app.use(cors({
-  origin: "https://delivery-system-puce.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
+
 app.use('/api', routes);
+
+// teste
+app.get('/', (req, res) => {
+  res.send('API rodando 🚀');
+});
 
 const PORT = process.env.PORT || 3001;
 
-async function createAdmin() {
-  // 🔥 força recriação
-  await prisma.user.deleteMany({
-    where: { email: "admin@admin.com" }
-  });
-
-  const hashedPassword = await bcrypt.hash("123456", 10);
-
-  await prisma.user.create({
-    data: {
-      name: "Admin",
-      email: "admin@admin.com",
-      password: hashedPassword,
-      role: "ADMIN"
-    }
-  });
-
-  console.log("🔥 Admin recriado");
-}
-
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
-  await createAdmin();
 });
