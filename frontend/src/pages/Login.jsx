4af50fc4bd66335b32ffd92c267c4a9c68b779bd
felipebretas03-creator <felipebretas03@gmail.com>
2026-04-14 15:40:30 -0300

    corrigindo login api

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('https://delivery-system-production-6da2.up.railway.app/auth/login', { email, password });
+      const res = await axios.post('https://delivery-system-production-6da2.up.railway.app/api/auth/login',  { email, password });
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('role', res.data.role);
       localStorage.setItem('name', res.data.name);
